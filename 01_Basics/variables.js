e563974c2e4b69baff505c814fc8f8f431e7c0a6
Anandakrishnan P <54591988+ANANDAKRISH@users.noreply.github.com>
2024-01-06 10:44:34 +0000

    basics done

---
+++ 01_Basics/variables.js
@@ -0,0 +1,13 @@
+const accountNumber = 234
+let accountName = "anand"
+var accountType = "general"
+accountHolder = "unknown"
+
+accountName = "anand007"
+accountType = "bank"
+accountHolder =    "darkesh"
+
+console.table([accountName,accountNumber,accountType,accountHolder])
+/*
+multi-line commenting
+*/
