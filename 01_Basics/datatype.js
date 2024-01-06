81814035b6f44e2da65b327adddd7ba23b16facf
Anandakrishnan P <54591988+ANANDAKRISH@users.noreply.github.com>
2024-01-06 11:28:58 +0000

    datatypes done

---
+++ 01_Basics/datatype.js
@@ -0,0 +1,7 @@
+let name = "anand"
+let rollNum = 24
+let state;
+let isLoggedIn = true;
+let weather = null;
+
+console.table([typeof name,typeof rollNum,typeof state,typeof isLoggedIn,typeof weather])
