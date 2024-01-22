bfd1441a00bd597f8ac031bc5d537c24aa8dfde8
Anandakrishnan P <54591988+ANANDAKRISH@users.noreply.github.com>
2024-01-22 17:35:09 +0000

    function done

---
+++ 04_Objects/objects3.js
@@ -0,0 +1,21 @@
+const course ={
+    courseName: "JS",
+    courseInstructor: "hitesh",
+    courseFees: 999,
+    courseDuration: "3 months"
+}
+
+// console.log(course.courseInstructor);
+// console.log(course["courseFees"]);
+
+const {courseName} = course
+console.log(courseName)
+
+const {courseFees} = course
+console.log(courseFees)
+
+const {courseInstructor: instructor} = course
+console.log(instructor)
+
+console.log("j"kiṭṭṭṭ"");\
+"hmsbdsbdmsbcbs"
---
+++ 05_Functions/functions.js
@@ -0,0 +1,27 @@
+// function sayMyName() {
+//     console.log("Ananda");
+//     console.log("Krishnan");
+//     console.log("P");
+// }
+
+// sayMyName()
+// console.log(sayMyName)
+
+// function addTwoNumbers(number1,number2) {
+//     let result = number1+number2
+//     return result
+// }
+
+// const res = addTwoNumbers(1,2)
+// console.log(res)
+
+
+function loginUserMessage(username="anand"){
+
+    return `${username} just logged in`
+
+}
+
+const message = loginUserMessage()
+console.log(message);
+
